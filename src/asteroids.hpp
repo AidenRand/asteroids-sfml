6f47efc5b1cf520b4df45aeec8fba4539efe0c22
aiden <aidenrphotos@gmail.com>
2023-05-24 12:24:21 -0400

    Add asteroids class

---
+++ src/asteroids.hpp
@@ -0,0 +1,12 @@
+#ifndef ASTEROIDS_HPP
+#define ASTEROIDS_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Asteroids
+{
+public:
+	Asteroids(float asteroid_x, float asteroid_y, int asteroid_speed);
+};
+
+#endif
